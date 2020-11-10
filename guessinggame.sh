9598246a5bd1452b23c655de9b396b648c5ea005
shaskum7 <74189763+shaskum7@users.noreply.github.com>
2020-11-10 06:39:18 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,18 +2,18 @@
 echo "Play Interesting Guessing Game:"
 echo "Please enter no of files present in current directory:-"
 ask_user_input () {
-    correct_no=$(ls -l | wc -l)
+    	  correct_no=$(ls -l | wc -l)
 	  read guess_no_of_line
 	  is_number
 }
 is_number () {
     re='^[0-9]+$'
-	  if [[ "$guess_no_of_line" =~ $re ]]
-	  then
-		    guess
-	  else
+	  while ![[ "$guess_no_of_line" =~ $re ]]
+	  do
 		    echo "Wrong input, please enter integer value:"
 		    ask_user_input
+	  done
+	  guess
 }
 guess () {
     if [[ $guess_no_of_line -ne $correct_no ]]
@@ -26,6 +26,7 @@ guess () {
 		    fi
 		    ask_user_input	
 	  else
-		    echo "Congratualtion, You entered correct value"	
+		    echo "Congratualtion, You entered correct value"
+    fi
 }
 ask_user_input
